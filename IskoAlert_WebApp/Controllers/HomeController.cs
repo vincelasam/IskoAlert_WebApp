f0f0ad4b1be64fa9b73355e20b4a7be5520dbe9b
Lian Paredes <lianluigiventura21@gmail.com>
2026-01-06 15:52:50 +0800

    Add MyReports action to HomeController

--- IskoAlert_WebApp/Controllers/HomeController.cs
+++ IskoAlert_WebApp/Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace IskolarAlert.Controllers
         {
             return View();
         }
+
+        public IActionResult MyReports()
+        {
+            return View();
+        }
     
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
