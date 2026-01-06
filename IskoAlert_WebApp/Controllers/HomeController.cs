2d6eaee158ed2bdb3e9267f5ccdea3b2e6b7eb4e
Lian Paredes <lianluigiventura21@gmail.com>
2026-01-06 16:22:35 +0800

    Update HomeController.cs

--- IskoAlert_WebApp/Controllers/HomeController.cs
+++ IskoAlert_WebApp/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace IskolarAlert.Controllers
         {
             return View();
         }
+
+        public IActionResult LostAndFound()
+        {
+            return View();
+        }
     
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
