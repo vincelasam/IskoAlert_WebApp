9ba54b4bc6846dae4328cb6eb35c8b677b7ac4eb
Lian Paredes <lianluigiventura21@gmail.com>
2026-01-06 15:31:21 +0800

    Add ReportIncident action to HomeController

--- IskoAlert_WebApp/Controllers/HomeController.cs
+++ IskoAlert_WebApp/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace IskolarAlert.Controllers
             return View();
         }
 
+        public IActionResult ReportIncident()
+        {
+            return View();
+        }
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
