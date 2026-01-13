0c7692e573349c363bbf703c06f4cf326b7fe640
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-14 01:17:14 +0800

    added

--- IskoAlert_WebApp/Services/Interfaces/IUserService.cs
+++ IskoAlert_WebApp/Services/Interfaces/IUserService.cs
@@ -1,3 +1,5 @@
+using IskoAlert_WebApp.Models.Domain;
+using IskoAlert_WebApp.Models.Domain.Enums;
 using IskoAlert_WebApp.Models.ViewModels.Account;
 
 namespace IskoAlert_WebApp.Services.Interfaces
@@ -5,5 +7,8 @@ namespace IskoAlert_WebApp.Services.Interfaces
     public interface IUserService
     {
         Task RegisterAsync(RegisterViewModel model);
+        Task<User?> ValidateCredentialsAsync(string email, string password, UserRole role);
     }
+
+
 }
