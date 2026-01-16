2b9723464709abe4545445f4aebf5ab019982492
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-16 20:27:12 +0800

    updated the contstructor

--- IskoAlert_WebApp/Models/Domain/LostFoundItem.cs
+++ IskoAlert_WebApp/Models/Domain/LostFoundItem.cs
@@ -17,14 +17,15 @@ namespace IskoAlert_WebApp.Models.Domain
         public string Email { get; private set; }
         public CampusLocation LocationFound { get; private set; }
         public DateTime DatePosted { get; private set; }
+        public DateTime? DateEdited { get; private set; }
         public ItemCategory Category { get; private set; }
         public ItemStatus Status { get; private set; }
-        public DateTime? ArchivedAt { get; set; }
+        public DateTime? ArchivedAt { get; private set; }
         public string ImagePath { get; private set; }
 
         protected LostFoundItem() { }
 
-        public LostFoundItem(string title, string description,ItemStatus status, string email, CampusLocation location, ItemCategory category,string image)
+        public LostFoundItem(int userId, string title, string description,ItemStatus status, string email, CampusLocation location, ItemCategory category,string image)
         {
             if (string.IsNullOrWhiteSpace(title))
                 throw new ArgumentException("Item name is required.");
@@ -33,6 +34,8 @@ namespace IskoAlert_WebApp.Models.Domain
             if (string.IsNullOrWhiteSpace(email))
                 throw new ArgumentException("Email is required.");
             if (string.IsNullOrWhiteSpace(image)) throw new ArgumentException("Image is required.");
+
+            UserId = userId;
             Title = title;
             Description = description;
             Email = email;
@@ -63,18 +66,22 @@ namespace IskoAlert_WebApp.Models.Domain
             ArchivedAt = DateTime.UtcNow;
         }
 
-        public void UpdateImage(string imagePath) => ImagePath = imagePath;
 
-        public void Update(string title, string description, CampusLocation location, ItemCategory category, ItemStatus status)
+        public void Update(string title, string description, ItemStatus status, string email, CampusLocation location, ItemCategory category, string image)
         {
             if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title is required.");
             if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Description is required.");
 
             Title = title;
             Description = description;
+            Email = email;
             LocationFound = location;
             Category = category;
-            Status = status;
+            ImagePath = image;
+
+            if (Status != status)
+                ChangeStatus(status);
+            DateEdited = DateTime.UtcNow;
         }
     }
 }
