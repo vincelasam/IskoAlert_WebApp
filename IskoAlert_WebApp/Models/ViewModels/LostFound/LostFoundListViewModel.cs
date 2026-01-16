751ddd4235c20a82c7db81e27f8ea68eec00cf39
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-16 16:11:58 +0800

    added the logic to view all the list of items

--- IskoAlert_WebApp/Models/ViewModels/LostFound/LostFoundListViewModel.cs
+++ IskoAlert_WebApp/Models/ViewModels/LostFound/LostFoundListViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace IskoAlert_WebApp.Models.ViewModels.LostFound
+﻿using IskoAlert_WebApp.Models.Domain.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IskoAlert_WebApp.Models.ViewModels.LostFound
 {
     public class LostFoundListViewModel
     {
+        // SEARCH / FILTER FIELDS
+        public string? SearchKeyword { get; set; }
+
+        public ItemStatus? SelectedStatus { get; set; }   // Lost / Found
+
+        public CampusLocation? SelectedCampusLocation { get; set; }
+
+        // Dropdown source
+        public IEnumerable<SelectListItem> CampusLocations { get; set; }
+            = new List<SelectListItem>();
+
+        // RESULTS (READ-ONLY)
+         
+        public List<LostFoundItemDisplayViewModel> Items { get; set; }
+            = new();
+
+        // PAGE CONTEXT 
+        public bool IsMyListingsPage { get; set; }
     }
 }
