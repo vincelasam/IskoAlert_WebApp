2a2e558994cf52b8810d1c3d281164e96547fee1
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-17 05:38:53 +0800

    added this helper

---
+++ IskoAlert_WebApp/Services/DropdownHelper.cs
@@ -0,0 +1,30 @@
+﻿using IskoAlert_WebApp.Models.Domain.Enums;
+using IskoAlert_WebApp.Models.ViewModels.LostFound;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IskoAlert_WebApp.Services
+{
+    public static class DropdownHelper
+    {
+        public static void PopulateLostFoundDropdowns(dynamic model)
+        {
+            // Category dropdown from ItemCategory enum
+            model.Category = Enum.GetValues(typeof(ItemCategory))
+                                 .Cast<ItemCategory>()
+                                 .Select(c => new SelectListItem
+                                 {
+                                     Text = c.ToString(),
+                                     Value = ((int)c).ToString()
+                                 }).ToList();
+
+            // Campus location dropdown from CampusLocation enum
+            model.CampusLocations = Enum.GetValues(typeof(CampusLocation))
+                                        .Cast<CampusLocation>()
+                                        .Select(l => new SelectListItem
+                                        {
+                                            Text = l.ToString(),
+                                            Value = ((int)l).ToString()
+                                        }).ToList();
+        }
+    }
+}
