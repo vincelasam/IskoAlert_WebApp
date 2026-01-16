2df3c4bdc2db93d69db34e1240016df1cdd742b3
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-16 16:12:10 +0800

    added the logic for creating an item

--- IskoAlert_WebApp/Services/Implementations/LostFoundService.cs
+++ IskoAlert_WebApp/Services/Implementations/LostFoundService.cs
@@ -1,22 +1,82 @@
 using IskoAlert_WebApp.Data;
 using IskoAlert_WebApp.Models.Domain;
+using IskoAlert_WebApp.Models.Domain.Enums;
 using IskoAlert_WebApp.Models.ViewModels.LostFound;
 using IskoAlert_WebApp.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using IskoAlert_WebApp.Models.Domain.Enums;
 
 namespace IskoAlert_WebApp.Services.Implementations
 {
     public class LostFoundService : ILostFoundService
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
         public LostFoundService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task CreateLostItemAsync(LostFoundItemViewModel model)
+        public LostFoundService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task CreateLostItemAsync(CreateItem model)
+        {
+            var photo = model.ImageFile;
+            if (photo == null || photo.Length == 0)
+                throw new ArgumentException("Image is required.");
+            
+            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(photo.FileName)}";
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "uploads/items", fileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await photo.CopyToAsync(stream);
+            }
+
+            string imagePath = $"/uploads/items/{fileName}";
+
+            var newItem = new LostFoundItem(
+                title: model.Title,
+                description: model.Description,
+                status: model.LostOrFound,
+                email: model.Email,
+                location: model.SelectedCampusLocation,
+                category: model.SelectedCategory,
+                image: imagePath
+                );
+     
+            //  Database operations
+            _context.LostFoundItems.Add(newItem);
+            await _context.SaveChangesAsync();
+        }
+
+
+
+        Task async UpdateItemStatusAsync(int itemId, ReportStatus newStatus, int userId)
+        {
+
+        }
+
+        Task async <List<LostFoundItem>> GetAllItemsAsync(string keyword)
+        {
+
+        }
+
+        Task async <List<LostFoundItem>> GetUserItemsAsync(int userId)
+        {
+
+        }
+
+        Task async <LostFoundItem> GetItemByIdAsync(int itemId)
+        {
+
+        }
+
+        Task async DeleteItemAsync(int itemId, int userId)
         {
 
         }
